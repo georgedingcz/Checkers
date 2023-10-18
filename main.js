56de8820bcb3e98e477794931715eed4aa9a1908
George Ding <dingchangzhao@hotmail.com>
2023-10-18 15:23:25 +0800

    refactor piece options
    change piece options from numbers to names for better clarity

--- main.js
+++ main.js
@@ -1,250 +1,331 @@
+//piece constants for readability
+const empty = 0;
+const greenPiece = 1;
+const redPiece = -1;
+const greenOption = 2;
+const redOption = -2;
+const optionalMove = 3;
+
 //game
 const game = {
   board: [],
   greenCount: 0,
   redCount: 0,
-}
+  turn: 0,
+};
 //cached elements
 const playerRedPieces = document.querySelector("#playerRedPieces");
 const playerGreenPieces = document.querySelector("#playerGreenPieces");
 const playerTurn = document.querySelector("#playerTurn");
-const boardSection = document.querySelector("#board")
-const winner = document.querySelector("#winner")
-const restart = document.querySelector("#restart")
+const boardSection = document.querySelector("#board");
+const winner = document.querySelector("#winner");
+const restart = document.querySelector("#restart");
 //functions
 //to initialise
-function initialise () {
+function initialise() {
   game.board = [
     [],
     [],
-    [-1,null,-1,null,-1,null,-1,null],
-    [null,-1,null,-1,null,-1,null,-1],
-    [-1,null,-1,null,-1,null,-1,null],
-    [null,0,null,0,null,0,null,0],
-    [0,null,0,null,0,null,0,null],
-    [null,1,null,1,null,1,null,1],
-    [1,null,1,null,1,null,1,null],
-    [null,1,null,1,null,1,null,1],
+    [redPiece, null, redPiece, null, redPiece, null, redPiece, null],
+    [null, redPiece, null, redPiece, null, redPiece, null, redPiece],
+    [redPiece, null, redPiece, null, redPiece, null, redPiece, null],
+    [null, empty, null, empty, null, empty, null, empty],
+    [empty, null, empty, null, empty, null, empty, null],
+    [null, greenPiece, null, greenPiece, null, greenPiece, null, greenPiece],
+    [greenPiece, null, greenPiece, null, greenPiece, null, greenPiece, null],
+    [null, greenPiece, null, greenPiece, null, greenPiece, null, greenPiece],
     [],
     [],
   ];
   game.turn = 1;
-  render()
-  winner.innerText = ""
-  playerTurn.hidden = false
+  render();
+  winner.innerText = "";
+  playerTurn.hidden = false;
 }
 //to clear alternative options
-function clear () {
+function clear() {
   //clear options
-  game.board.forEach(function (boardRow, rowIndex){
-    boardRow.forEach(function(piece, columnIndex) {
-      if (piece === 3 || piece === 2 || piece === -2 || piece === -3) {
-        game.board[rowIndex][columnIndex] = 0
+  game.board.forEach(function (boardRow, rowIndex) {
+    boardRow.forEach(function (piece, columnIndex) {
+      switch (piece) {
+        case optionalMove:
+        case greenOption:
+        case redOption:
+          game.board[rowIndex][columnIndex] = empty;
+          break;
       }
-    })
-  })
+    });
+  });
 }
 //to restore state of existing checked piece
-function antiOtherOption () {
-  game.board.forEach(function (boardRow, rowIndex){
-    boardRow.forEach(function(piece, columnIndex) {
+function antiOtherOption() {
+  game.board.forEach(function (boardRow, rowIndex) {
+    boardRow.forEach(function (piece, columnIndex) {
       if (game.turn === 1) {
-        if (piece === 3) {
-          game.board[rowIndex][columnIndex] = 0
-        } else if (piece === 2) {
-          game.board[rowIndex][columnIndex] = 1
+        if (piece === optionalMove) {
+          game.board[rowIndex][columnIndex] = empty;
+        } else if (piece === greenOption) {
+          game.board[rowIndex][columnIndex] = greenPiece;
         }
       } else if (game.turn === -1) {
-        if (piece === 3) {
-          game.board[rowIndex][columnIndex] = 0
-        } else if (piece === -2) {
-          game.board[rowIndex][columnIndex] = -1
+        if (piece === optionalMove) {
+          game.board[rowIndex][columnIndex] = empty;
+        } else if (piece === redOption) {
+          game.board[rowIndex][columnIndex] = redPiece;
         }
       }
-    })
-  })
+    });
+  });
 }
 //when green piece reaches the opposite end
-function greenReached () {
-  winner.innerText = "Player Green has won"
-  playerTurn.hidden = true
+function greenReached() {
+  winner.innerText = "Player Green has won";
+  playerTurn.hidden = true;
 }
 //when red piece reaches the opposite end
-function redReached () {
-  winner.innerText = "Player Red has won"
-  playerTurn.hidden = true
+function redReached() {
+  winner.innerText = "Player Red has won";
+  playerTurn.hidden = true;
 }
 //renders
 //render the board
-function renderBoard () {
+function renderBoard() {
   //clear the board
   while (boardSection.firstChild) {
-    boardSection.removeChild(boardSection.firstChild)
+    boardSection.removeChild(boardSection.firstChild);
   }
   //build the board
-  game.board.forEach ((row,rowIndex) => {
-      row.forEach((column, columnIndex) => {
-        const squareElement = document.createElement("div")
-        boardSection.append(squareElement)
-        //add square colours
-        squareElement.classList.add("whiteSquare");
-        if (rowIndex % 2 === 0 && columnIndex % 2 === 0) {
-          squareElement.classList.remove("whiteSquare");
-          squareElement.classList.add("blackSquare");
-        } else if (rowIndex % 2 !== 0 && columnIndex % 2 !== 0) {
-          squareElement.classList.remove("whiteSquare");
-          squareElement.classList.add("blackSquare");
-        }
-        const checkerPieces = document.createElement("div")
-        squareElement.append(checkerPieces)
-        checkerPieces.setAttribute("row", rowIndex)
-        checkerPieces.setAttribute("column", columnIndex)
-        //add piece colours
-        if (game.board[rowIndex][columnIndex] === -1) {
-          checkerPieces.classList.add("redCheckersPiece")
-        } else if (game.board[rowIndex][columnIndex] === 1) {
-          checkerPieces.classList.add("greenCheckersPiece")
-        } else if (game.board[rowIndex][columnIndex] === 0) {
-          checkerPieces.classList.add("noCheckersPiece")
-        } else if (game.board[rowIndex][columnIndex] === 3) {
-          checkerPieces.classList.add("optionCheckersPiece")
-        } else if (game.board[rowIndex][columnIndex] === -2) {
-          checkerPieces.classList.add("optionRedCheckersPiece")
-        } else if (game.board[rowIndex][columnIndex] === 2) {
-          checkerPieces.classList.add("optionGreenCheckersPiece")
-        } 
-      })
-  })
+  game.board.forEach((row, rowIndex) => {
+    row.forEach((column, columnIndex) => {
+      const squareElement = document.createElement("div");
+      boardSection.append(squareElement);
+      //add square colours
+      squareElement.classList.add("whiteSquare");
+      if (rowIndex % 2 === 0 && columnIndex % 2 === 0) {
+        squareElement.classList.remove("whiteSquare");
+        squareElement.classList.add("blackSquare");
+      } else if (rowIndex % 2 !== 0 && columnIndex % 2 !== 0) {
+        squareElement.classList.remove("whiteSquare");
+        squareElement.classList.add("blackSquare");
+      }
+      const checkerPieces = document.createElement("div");
+      squareElement.append(checkerPieces);
+      checkerPieces.setAttribute("row", rowIndex);
+      checkerPieces.setAttribute("column", columnIndex);
+      //add piece colours
+      if (game.board[rowIndex][columnIndex] === redPiece) {
+        checkerPieces.classList.add("redCheckersPiece");
+      } else if (game.board[rowIndex][columnIndex] === greenPiece) {
+        checkerPieces.classList.add("greenCheckersPiece");
+      } else if (game.board[rowIndex][columnIndex] === empty) {
+        checkerPieces.classList.add("noCheckersPiece");
+      } else if (game.board[rowIndex][columnIndex] === optionalMove) {
+        checkerPieces.classList.add("optionCheckersPiece");
+      } else if (game.board[rowIndex][columnIndex] === redOption) {
+        checkerPieces.classList.add("optionRedCheckersPiece");
+      } else if (game.board[rowIndex][columnIndex] === greenOption) {
+        checkerPieces.classList.add("optionGreenCheckersPiece");
+      }
+    });
+  });
 }
 //render remaining pieces
-function renderRemaining () {
-  game.redCount = 0
-  game.greenCount = 0
-  game.board.forEach(function (boardRow){
-    boardRow.forEach(function(piece) {
-      if (piece === 1 || piece === 2) {
-        game.greenCount ++
-      } else if (piece === -1 || piece === -2) {
-        game.redCount ++
+function renderRemaining() {
+  game.redCount = 0;
+  game.greenCount = 0;
+  game.board.forEach(function (boardRow) {
+    boardRow.forEach(function (piece) {
+      switch (piece) {
+        case greenPiece:
+        case greenOption:
+          game.greenCount++;
+          break;
+        case redPiece:
+        case redOption:
+          game.redCount++;
+          break;
       }
-    })
-  })
-  playerRedPieces.innerText = `:${game.redCount}`
-  playerGreenPieces.innerText = `:${game.greenCount}`
-  return game.redCount, game.greenCount
+    });
+  });
+  playerRedPieces.innerText = `:${game.redCount}`;
+  playerGreenPieces.innerText = `:${game.greenCount}`;
+  return game.redCount, game.greenCount;
 }
 //render player turn
-function renderTurn () {
+function renderTurn() {
   if (game.turn === 1) {
-    playerTurn.innerText = "Player Green's turn"
+    playerTurn.innerText = "Player Green's turn";
   } else if (game.turn === -1) {
-    playerTurn.innerText = "Player Red's turn"
+    playerTurn.innerText = "Player Red's turn";
   }
 }
 //render winner
-function renderWinner () {
+function renderWinner() {
   if (game.greenCount === 0) {
-    winner.innerText = "Player Red has won"
-    playerTurn.hidden = true
+    winner.innerText = "Player Red has won";
+    playerTurn.hidden = true;
   } else if (game.redCount === 0) {
-    winner.innerText = "Player Green has won"
-    playerTurn.hidden = true
+    winner.innerText = "Player Green has won";
+    playerTurn.hidden = true;
   }
 }
 //to render all at once
-function render () {
-  renderBoard()
-  renderRemaining()
-  renderTurn()
-  renderWinner()
+function render() {
+  renderBoard();
+  renderRemaining();
+  renderTurn();
+  renderWinner();
 }
 //add event listener for the board
-boardSection.addEventListener("click", handleClick)
-restart.addEventListener("click", initialise)
+boardSection.addEventListener("click", handleClick);
+restart.addEventListener("click", initialise);
 //handle click
-function handleClick (e) {
-  const itemClicked = e.target
-  const r = parseInt(itemClicked.getAttribute("row"))
-  const c = parseInt(itemClicked.getAttribute("column"))
+function handleClick(e) {
+  const itemClicked = e.target;
+  const r = parseInt(itemClicked.getAttribute("row"));
+  const c = parseInt(itemClicked.getAttribute("column"));
   //to check and change one square diagonally away
-  function oneSquareDiagonal (rM, cM, checkOneSpace, makeOneSpace) {
+  function oneSquareDiagonal(rM, cM, checkOneSpace, makeOneSpace) {
     if (game.board[r + rM][c + cM] === checkOneSpace) {
-      game.board[r + rM][c + cM] = makeOneSpace
+      game.board[r + rM][c + cM] = makeOneSpace;
     }
   }
   //to check and change two squares diagonally away
-  function twoSquaresDiagonal (rM, cM, checkOneSpace, makeOneSpace, checkTwoSpaces, makeTwoSpaces) {
+  function twoSquaresDiagonal(
+    rM,
+    cM,
+    checkOneSpace,
+    makeOneSpace,
+    checkTwoSpaces,
+    makeTwoSpaces
+  ) {
     if (game.board[r + rM][c + cM] === checkOneSpace) {
-      if (game.board[r + 2*rM][c + 2*cM] === checkTwoSpaces) {
-        game.board[r + rM][c + cM] = makeOneSpace
-        game.board[r + 2*rM][c + 2*cM] = makeTwoSpaces
+      if (game.board[r + 2 * rM][c + 2 * cM] === checkTwoSpaces) {
+        game.board[r + rM][c + cM] = makeOneSpace;
+        game.board[r + 2 * rM][c + 2 * cM] = makeTwoSpaces;
       }
     }
   }
   //movements and options
   if (game.turn === 1) {
     //to show options
-    if (game.board[r][c] === 1) {
-      antiOtherOption()
+    if (game.board[r][c] === greenPiece) {
+      antiOtherOption();
 
-      oneSquareDiagonal(-1,-1,0,3)
-      oneSquareDiagonal(-1,1,0,3)
+      oneSquareDiagonal(redPiece, redPiece, empty, optionalMove);
+      oneSquareDiagonal(redPiece, greenPiece, empty, optionalMove);
 
-      twoSquaresDiagonal(-1,-1,-1,-1,0,3)
-      twoSquaresDiagonal(-1,1,-1,-1,0,3)
-      game.board[r][c] = 2
-    //to choose an option
-    } else if (game.board[r][c] === 3) {
+      twoSquaresDiagonal(
+        redPiece,
+        redPiece,
+        redPiece,
+        redPiece,
+        empty,
+        optionalMove
+      );
+      twoSquaresDiagonal(
+        redPiece,
+        greenPiece,
+        redPiece,
+        redPiece,
+        empty,
+        optionalMove
+      );
+      game.board[r][c] = greenOption;
+      //to choose an option
+    } else if (game.board[r][c] === optionalMove) {
       if (r === 2) {
-        greenReached()
+        greenReached();
       }
-      oneSquareDiagonal(1,1,2,0)
-      oneSquareDiagonal(1,-1,2,0)
+      oneSquareDiagonal(greenPiece, greenPiece, greenOption, empty);
+      oneSquareDiagonal(greenPiece, redPiece, greenOption, empty);
 
-      twoSquaresDiagonal(1,1,-1,0,2,0)
-      twoSquaresDiagonal(1,-1,-1,0,2,0)
+      twoSquaresDiagonal(
+        greenPiece,
+        greenPiece,
+        redPiece,
+        empty,
+        greenOption,
+        empty
+      );
+      twoSquaresDiagonal(
+        greenPiece,
+        redPiece,
+        redPiece,
+        empty,
+        greenOption,
+        empty
+      );
 
-      game.board[r][c] = 1
-      clear() 
-      game.turn = -1 
-    //to uncheck a piece
-    } else if (game.board[r][c] === 2) {
-      game.board[r][c] = 1
-      clear()
+      game.board[r][c] = greenPiece;
+      clear();
+      game.turn = -1;
+      //to uncheck a piece
+    } else if (game.board[r][c] === greenOption) {
+      game.board[r][c] = greenPiece;
+      clear();
     }
   } else if (game.turn === -1) {
     //to show options
-    if (game.board[r][c] === -1) {
-      antiOtherOption()
+    if (game.board[r][c] === redPiece) {
+      antiOtherOption();
 
-      oneSquareDiagonal(1,-1,0,3)
-      oneSquareDiagonal(1,1,0,3)
+      oneSquareDiagonal(greenPiece, redPiece, empty, optionalMove);
+      oneSquareDiagonal(greenPiece, greenPiece, empty, optionalMove);
 
-      twoSquaresDiagonal(1,-1,1,1,0,3)
-      twoSquaresDiagonal(1,1,1,1,0,3)
+      twoSquaresDiagonal(
+        greenPiece,
+        redPiece,
+        greenPiece,
+        greenPiece,
+        empty,
+        optionalMove
+      );
+      twoSquaresDiagonal(
+        greenPiece,
+        greenPiece,
+        greenPiece,
+        greenPiece,
+        empty,
+        optionalMove
+      );
 
-      game.board[r][c] = -2
-    //to choose an option
-    } else if (game.board[r][c] === 3) {
+      game.board[r][c] = redOption;
+      //to choose an option
+    } else if (game.board[r][c] === optionalMove) {
       if (r === 9) {
-        redReached()
+        redReached();
       }
-      oneSquareDiagonal(-1,1,-2,0)
-      oneSquareDiagonal(-1,-1,-2,0)
+      oneSquareDiagonal(redPiece, greenPiece, redOption, empty);
+      oneSquareDiagonal(redPiece, redPiece, redOption, empty);
 
-      twoSquaresDiagonal(-1,1,1,0,-2,0)
-      twoSquaresDiagonal(-1,-1,1,0,-2,0)
+      twoSquaresDiagonal(
+        redPiece,
+        greenPiece,
+        greenPiece,
+        empty,
+        redOption,
+        empty
+      );
+      twoSquaresDiagonal(
+        redPiece,
+        redPiece,
+        greenPiece,
+        empty,
+        redOption,
+        empty
+      );
 
-      game.board[r][c] = -1
-      clear() 
-      game.turn = 1
-    //to uncheck a piece
-    } else if (game.board[r][c] === -2) {
-      game.board[r][c] = -1
-      clear()
+      game.board[r][c] = redPiece;
+      clear();
+      game.turn = 1;
+      //to uncheck a piece
+    } else if (game.board[r][c] === redOption) {
+      game.board[r][c] = redPiece;
+      clear();
     }
   }
-  render()
+  render();
 }
 //initialise the game
-initialise()
+initialise();
